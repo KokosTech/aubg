308b1c06bba9cf0555d92344a50d8307bcb33b37
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2025-10-16 18:18:47 +0300

    proj2: comments Accounts

--- cos_2030_java/Project2/src/Accounts.java
+++ cos_2030_java/Project2/src/Accounts.java
@@ -13,9 +13,9 @@ public class Accounts {
      * Standard constructor for objects of class Guild
      */
     Accounts() {
-        taxFund = 0;
-        donationFund = 0;
-        darkFund = 0;
+        taxFund = 0.0;
+        donationFund = 0.0;
+        darkFund = 0.0;
     }
 
     /**
@@ -33,35 +33,74 @@ public class Accounts {
         return false;
     }
 
+    /**
+     * getTaxFund - this method returns the current amount of money in the Tax Fund
+     *
+     * @return double taxFund
+     */
     public double getTaxFund() {
         return taxFund;
     }
 
+    /**
+     * getDonationFund - this method returns the current amount of money in the Donation Fund
+     *
+     * @return double donationFund
+     */
     public double getDonationFund() {
         return donationFund;
     }
 
+    /**
+     * getDarkFund - this method returns the current amount of money in the Dark Fund
+     *
+     * @return double darkFund
+     */
     public double getDarkFund() {
         return darkFund;
     }
 
     // it feels so illegal to use $ as a variable name ;-;
 
+    /**
+     * addToTaxFund - this method adds a specified amount to the Tax Fund
+     *
+     * @param $ double - amount to add to the Tax Fund
+     * @return nothing
+     */
     public void addToTaxFund(double $) {
         if (amountChecks($)) return;
         this.taxFund += $;
     }
 
+    /**
+     * addToDonationFund - this method adds a specified amount to the Donation Fund
+     *
+     * @param $ double - amount to add to the Donation Fund
+     * @return nothing
+     */
     public void addToDonationFund(double $) {
         if (amountChecks($)) return;
         this.donationFund += $;
     }
 
+    /**
+     * addToDarkFund - this method adds a specified amount to the Dark Fund
+     *
+     * @param $ double - amount to add to the Dark Fund
+     * @return nothing
+     */
     public void addToDarkFund(double $) {
         if (amountChecks($)) return;
         this.darkFund += $;
     }
 
+
+    /**
+     * allFunds - this method returns a sum of all funds
+     *
+     * @return double - sum of all funds
+     */
     public double allFunds() {
         return this.taxFund + this.donationFund + this.darkFund;
     }
