6cb308195b2ca018733e9df192b906a2607d2b7a
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2025-12-08 02:58:37 +0200

    java: proj6 init

---
+++ cos_2030_java/Project6/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module doychinov.kaloyan.project6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens doychinov.kaloyan.project6 to javafx.fxml;
+    exports doychinov.kaloyan.project6;
+}
