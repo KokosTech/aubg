24a19502e61650b59e216460603334fbec6c120f
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2026-04-25 17:36:18 +0300

    improve: config parsing

--- cos_3040_python/project/utils/config.py
+++ cos_3040_python/project/utils/config.py
@@ -1,16 +1,22 @@
 import configparser
+import os
 
-config = configparser.ConfigParser()
+RAILWAYS_FILE = "railways.json"
+TRAINS_FILE = "trains.json"
+MIN_TRANSFER_TIME = 5
+DEFAULT_CARRIAGE_CAPACITY = 60
 
-try:
-    with open("config.ini", "r") as f:
-        config.read_file(f)
-except FileNotFoundError:
-    print("Error: config.ini file not found. Please create a config.ini file with the necessary settings.")
-    exit(1)
+if os.path.exists("config.ini") and os.path.isfile("config.ini"):
+    config = configparser.ConfigParser()
 
-RAILWAYS_FILE = config["paths"]["railways_file"]
-TRAINS_FILE = config["paths"]["trains_file"]
-MIN_TRANSFER_TIME = config.getint("train", "min_transfer_time")
-
-DEFAULT_CARRIAGE_CAPACITY = config.getint("train", "default_carriage_capacity")
+    try:
+        with open("config.ini", "r") as f:
+            config.read_file(f)
+    except FileNotFoundError, IOError:
+        print("Error: Problem reading config.ini file.")
+        exit(1)
+        
+    RAILWAYS_FILE = config.get("paths", "railways_file", fallback=RAILWAYS_FILE)
+    TRAINS_FILE = config.get("paths", "trains_file", fallback=TRAINS_FILE)
+    MIN_TRANSFER_TIME = config.getint("train", "min_transfer_time", fallback=MIN_TRANSFER_TIME)
+    DEFAULT_CARRIAGE_CAPACITY = config.getint("train", "default_carriage_capacity", fallback=DEFAULT_CARRIAGE_CAPACITY)
